3aa4600a10ce408f517cec4aca8cc3b6dd781a41
atropos <sv7n@pm.me>
2024-10-23 21:17:34 +0100

    On conflict do nothing on event instert.

--- events/query.sql
+++ events/query.sql
@@ -1,5 +1,5 @@
 -- name: InsertEvent :one
-INSERT INTO events (id, source, type, specversion, datacontenttype, data, time, subject) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;
+INSERT INTO events (id, source, type, specversion, datacontenttype, data, time, subject) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT (id) DO NOTHING RETURNING *;
 
 -- name: GetEvent :one
 SELECT * FROM events WHERE id = $1;
